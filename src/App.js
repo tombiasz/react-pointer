17debd031920c6448ad85b4342797713e8291caf
tombiasz <tomasz.m.nowacki@gmail.com>
2018-03-04 14:41:10 +0100

    add map componenet

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import Map from './components/Map';
+
+
 class App extends Component {
+
   render() {
+    const mapApiKey = process.env.REACT_APP_GOOGLE_MAP_API_KEY
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="App" style={{ height: `100%`}}>
+        <Map
+          isMarkerShown
+          googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${mapApiKey}&v=3.exp&libraries=geometry,drawing,places`}
+          loadingElement={<div style={{ height: `100%` }} />}
+          containerElement={<div style={{ height: `100vh` }} />}
+          mapElement={<div style={{ height: `100%` }} />}
+        />
       </div>
     );
   }
