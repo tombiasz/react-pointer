6e2dff36a12d311026ca87e75c37fc4471629289
tombiasz <tomasz.m.nowacki@gmail.com>
2018-03-19 18:37:53 +0100

    add base top nav

--- src/App.js
+++ src/App.js
@@ -8,8 +8,21 @@ class App extends Component {
 
     render() {
       return (
-        <div className="App" style={{ height: `100%`}}>
+        <div className="">
+          <nav className="blue darken-2">
+            <div className="nav-wrapper">
+              <a href="#" className="brand-logo">Logo</a>
+              <ul id="nav-mobile" className="right hide-on-med-and-down">
+                <li><a href="sass.html">Sass</a></li>
+                <li><a href="badges.html">Components</a></li>
+                <li><a href="collapsible.html">JavaScript</a></li>
+              </ul>
+            </div>
+          </nav>
+
+          <div style={{height: '100%'}} >
             <MapContainer />
+          </div>
         </div>
       );
     }
--- src/index.js
+++ src/index.js
@@ -4,5 +4,8 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+import 'materialize-css';
+import 'materialize-css/dist/css/materialize.min.css';
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
