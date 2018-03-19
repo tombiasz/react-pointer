6a1ba67a53df72a213a39e75ca3f41993f313bbd
tombiasz <tomasz.m.nowacki@gmail.com>
2018-03-19 18:14:51 +0100

    revert and remove material-ui-next

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
 
-import AppBar from 'material-ui/AppBar';
-import Toolbar from 'material-ui/Toolbar';
-import Typography from 'material-ui/Typography';
-
 import MapContainer from './containers/MapContainer';
 
 
@@ -13,15 +9,7 @@ class App extends Component {
     render() {
       return (
         <div className="App" style={{ height: `100%`}}>
-
-          <AppBar position="static" color="default">
-            <Toolbar>
-              <Typography variant="title" color="inherit">
-                PoInter
-              </Typography>
-            </Toolbar>
-          </AppBar>
-          <MapContainer />
+            <MapContainer />
         </div>
       );
     }
