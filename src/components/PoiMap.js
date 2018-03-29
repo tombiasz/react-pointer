eb74c51db339923e7406ddcaee95d7d51e507d3f
tombiasz <tomasz.m.nowacki@gmail.com>
2018-03-29 15:51:25 +0200

    use es6 destructing

--- src/components/PoiMap.js
+++ src/components/PoiMap.js
@@ -1,24 +1,23 @@
 import React from 'react';
+
 import { withScriptjs, withGoogleMap, GoogleMap, Marker } from 'react-google-maps';
 
 
-const PoiMap = props => (
-    <GoogleMap
-        ref={props.setMapRef}
-        defaultZoom={8}
-        defaultCenter={{ lat: -34.397, lng: 150.644 }}
-    >
-        {props.pois.map((poi) => {
-            return <Marker
-                key={poi.id}
-                position={{
-                    lat: poi.point.coordinates[1],
-                    lng: poi.point.coordinates[0]
-                }}
-                animation={(props.animatePoi && props.animatePoi.id === poi.id) ? window.google.maps.Animation.BOUNCE : null}
-            />
-        })}
-    </GoogleMap>
-)
+const PoiMap = ({ pois, animatePoi, setMapRef }) => (
+  <GoogleMap
+    ref={setMapRef}
+    defaultZoom={8}
+    defaultCenter={{ lat: -34.397, lng: 150.644 }}
+  >
+    {pois.map((poi) => {
+      const [lng, lat] = poi.point.coordinates;
+      return <Marker
+        key={poi.id}
+        position={{ lat, lng }}
+        animation={(animatePoi && animatePoi.id === poi.id) ? window.google.maps.Animation.BOUNCE : null}
+      />
+    })}
+  </GoogleMap>
+);
 
 export default withScriptjs(withGoogleMap(PoiMap));
