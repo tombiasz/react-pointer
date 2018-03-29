3617ac0d79e65acb62fef76a6c0e8917433bd464
tombiasz <tomasz.m.nowacki@gmail.com>
2018-03-29 15:26:37 +0200

    use es6 destructing

--- src/components/PoiCard.js
+++ src/components/PoiCard.js
@@ -1,16 +1,16 @@
 import React from 'react';
 
 
-const PoiCard = props => (
+const PoiCard = ({ poi, onMouseEnter, onMouseLeave, onClick }) => (
   <div className="row"
-    key={ props.poi.id }
-    onMouseEnter={ props.onMouseEnter }
-    onMouseLeave={ props.onMouseLeave }
-    onClick={ props.onClick }
+    key={poi.id}
+    onMouseEnter={onMouseEnter}
+    onMouseLeave={onMouseLeave}
+    onClick={onClick}
   >
     <div className="col s12">
       <div className="card-panel hoverable">
-        <span className="card-title">{ props.poi.name }</span>
+        <span className="card-title">{poi.name}</span>
         <p>I am a very simple card. I am good at containing small bits of information.
         I am convenient because I require little markup to use effectively.</p>
       </div>
