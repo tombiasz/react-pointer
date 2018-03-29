9351977e2f031bd5480f6f0dea1f138226042ba3
tombiasz <tomasz.m.nowacki@gmail.com>
2018-03-29 17:30:33 +0200

    set app name

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,14 +1,15 @@
 import React from 'react';
 
+
 const NavBar = () => (
   <div className="navbar-fixed">
     <nav className="blue darken-2 z-depth-0">
       <div className="nav-wrapper">
-        <a href="#" className="brand-logo">Logo</a>
+        <a href="#" className="brand-logo">POInter</a>
         <ul id="nav-mobile" className="right hide-on-med-and-down">
-          <li><a href="sass.html">Sass</a></li>
-          <li><a href="badges.html">Components</a></li>
-          <li><a href="collapsible.html">JavaScript</a></li>
+          <li><a href="menu1.html">menu1</a></li>
+          <li><a href="menu2.html">menu2</a></li>
+          <li><a href="menu3.html">menu3</a></li>
         </ul>
       </div>
     </nav>
